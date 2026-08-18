a905f23aeee080bf0cef29f0535b715dc09a7c6c
Azzam Alrashed <azzam.rar@gmail.com>
2026-08-18 07:41:33 +0300

    Add iOS and macOS app projects

---
+++ iOS/caocap/caocap/caocapApp.swift
@@ -0,0 +1,17 @@
+//
+//  caocapApp.swift
+//  caocap
+//
+//  Created by Azzam Alrashed on 18/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct caocapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
